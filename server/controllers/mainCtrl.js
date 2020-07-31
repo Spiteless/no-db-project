6ae9eac3f1e3f51461ed877b32c06d91b8abf4be
Trillium <spiteless@gmail.com>
2020-07-31 12:37:41 -0700

    changed endpoints to api.users and api.events, moved controllers to mainCtrl to prevent issues are import of data file

---
+++ server/controllers/mainCtrl.js
@@ -0,0 +1,87 @@
+const data = require('../../data/data.json')
+let nextUserID = data.users.length + 1
+let nextEventID = data.events.length + 1
+
+/* user data:
+        "id": 1,
+        "first_name": "Stanly",
+        "last_name": "Woodland",
+        "email": "swoodland0@vistaprint.com",
+        "gender": "Male",
+        "paid": true
+*/
+
+const userCtrl = {
+    get: (req, res) => {
+        return res.status(200).send(data.users)
+    },
+    post: (req, res) => {
+        const newUser = {
+            "id": nextUserID,
+            ...req.body.newUser
+        }
+        nextUserID++
+        console.log(newUser)
+        data.users.push(newUser)
+        return res.status(200).send(data.users)
+    },
+    put: (req, res) => {
+        const {id} = req.params
+        const {newUser} = req.body
+        const index = data.users.findIndex( (u) => u.id === +id)
+        data.users[index] = {
+            ...data.users[index],
+            ...newUser,
+        }
+        return res.status(200).send(data.users)
+    },
+    delete: (req, res) => {
+        const {id} = req.params
+        const index = data.users.findIndex( u => u.id === +id)
+        if (index === -1) { res.status(404).send("No such user") }
+        else { data.users.splice(index, 1) 
+            return res.status(200).send(data.users)
+        }
+    },
+}
+
+const eventsCtrl = {
+    get: (req, res) => {
+        return res.status(200).send(data.events)
+    },
+    post: (req, res) => {
+        const newEvent = {
+            "id": nextEventID,
+            ...req.body.newEvent
+        }
+        nextEventID++
+        console.log(newEvent)
+        data.events.push(newEvent)
+        return res.status(200).send(data.events)
+    },
+    put: (req, res) => {
+        const {id} = req.params
+        const {newEvent} = req.body
+        const index = data.users.findIndex( (e) => e.id === +id)
+        data.events[index] = {
+            ...data.events[index],
+            ...newEvent,
+        }
+        return res.status(200).send(data.events)
+    },
+    delete: (req, res) => {
+        const {id} = req.params
+        const index = data.events.findIndex( e => E.id === +id)
+        if (index === -1) { res.status(404).send("No such event") }
+        else { data.events.splice(index, 1) 
+            return res.status(200).send(data.events)
+        }
+    },
+}
+
+
+
+module.exports = {
+    users: userCtrl,
+    events: eventsCtrl
+}
---
+++ server/index.js
@@ -0,0 +1,25 @@
+const express = require('express')
+
+const app = express()
+const port = 3333
+
+const ctrl = require('./controllers/mainCtrl')
+
+app.use(express.json())
+
+const api = { users:'/api/users/',
+              events:'/api/users/' }
+
+//users endpoints
+app.get(api.users, ctrl.users.get)
+app.post(api.users, ctrl.users.post)
+app.put(api.users + ":id", ctrl.users.put)
+app.delete(api.users + ":id", ctrl.users.delete)
+
+//events endpoints
+app.get(api.events, ctrl.users.get)
+app.post(api.events, ctrl.users.post)
+app.put(api.events + ":id", ctrl.users.put)
+app.delete(api.events + ":id", ctrl.users.delete)
+
+app.listen(port, () => console.log(`Server running on ${port}`))
